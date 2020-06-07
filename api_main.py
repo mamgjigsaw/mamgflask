2b89a560f722d2bd95b29406593cb6810a046e94
Miguel Martinez <mamg.sept30@gmail.com>
2020-06-07 14:46:07 -0600

    begin api

---
+++ api_main.py
@@ -0,0 +1,140 @@
+from flask import Flask,jsonify, request, make_response
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisissecrret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mamg/Documents/projects/test/api_flask/todo.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text =  db.Column(db.String(50))
+    complete = db.Column(db.Boolean)
+
+#db.create_all()
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args. **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+
+        try:
+            data = jwt.encode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id'])
+        except:
+            return jsonify({'message':'Token is invalid'}), 401
+
+        return f(current_user, *args, **kwargs)
+    
+    return decorated
+
+#routes
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id':user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'toke': token.decode('UTF-8')})
+    else:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+@app.route('/user', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+    return jsonify({'users':output})
+
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message':'No user found'})
+    
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsonify({'user':user_data})
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message':'New user was created'})
+
+@app.route('/user/<public_id>', methods=['PUT'])
+def promote_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message':'No user found'})
+    
+    user.admin = True
+    db.session.commit()
+
+    return jsonify({'message':'The user has been promoted'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+def delete_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message':'No user found'})
+    
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message':'The user has been deleted'})
+
+
+@app.route("/", methods =['POST'])
+def home():
+    return jsonify({"response":"hola"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
